8a66918f99e4eddc250f6721f400d577d668d231
lyg1597 <lyg1597@gmail.com>
2025-03-12 10:15:12 -0500

    get first batch of data

---
+++ nerf_exp4/res_vis.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+if __name__ == "__main__":
+    data = np.load('dozer2_pi_1000.npz')
+    images_lb = data['images_lb']
+    images_ub = data['images_ub']
+    camera_poses = data['images_noptb']
+
+    for i in range(1000):
+        print(camera_poses[i])
+        plt.figure(0)
+        plt.imshow(images_lb[i])
+        plt.figure(1)
+        plt.imshow(images_ub[i])
+        plt.show()
